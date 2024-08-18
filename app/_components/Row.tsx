5888065eee8c66fbef5c02c4b16508aaf544a340
Gerry Pass <rgpass@gmail.com>
2024-08-18 12:44:38 -0400

    Move size to full screen

--- app/_components/Row.tsx
+++ app/_components/Row.tsx
@@ -2,7 +2,7 @@ import { Square } from "./Square";
 
 export function Row({ row }: { row: string[] }) {
   return (
-    <div className="flex max-h-[calc(min(90vh,_90vw))] max-w-[calc(min(90vh,_90vw))] first-of-type:border-t-2 last-of-type:border-b-2 border-red-500">
+    <div className="flex max-h-[calc(min(100vh,_100vw))] max-w-[calc(min(100vh,_100vw))] first-of-type:border-t-2 last-of-type:border-b-2 border-red-500">
       {row.map((word) => (
         <Square key={word} word={word} />
       ))}
