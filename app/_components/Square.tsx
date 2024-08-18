eb66244cc3636aeb8d9106bca322382306d27072
Gerry Pass <rgpass@gmail.com>
2024-08-18 13:15:12 -0400

    Make text bigger

--- app/_components/Square.tsx
+++ app/_components/Square.tsx
@@ -15,7 +15,7 @@ export function Square({ word }: { word: string }) {
       className="relative flex justify-center items-center text-center w-full h-full aspect-square max-h-[calc(min(19vh,_19vw))] max-w-[calc(min(19vh,_19vw))] first-of-type:border-l-4 last-of-type:border-r-4 border-2 border-blue-500"
       onClick={handleClick}
     >
-      {word}
+      <span className="text-xl">{word}</span>
       {isSelected && (
         <div className="absolute w-full h-full border-8 rounded-full border-red-500 bg-red-500 opacity-25" />
       )}
