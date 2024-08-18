a09238205d58e21b676ce0eff1039889a005e54f
Gerry Pass <rgpass@gmail.com>
2024-08-18 13:27:28 -0400

    Add filtered words

--- app/host/_components/NextWord.tsx
+++ app/host/_components/NextWord.tsx
@@ -9,21 +9,31 @@ function randomOrder(arr: string[]) {
 
 export function NextWord() {
   const [remainingWords, setRemainingWords] = useState(randomOrder(words));
+  const [previousWords, setPreviousWords] = useState<string[]>([]);
   const [word, setWord] = useState("");
 
-  const handleClick = () => {
+  const handleClickNext = () => {
     const [first, ...rest] = remainingWords;
     setWord(first);
+    setPreviousWords([...previousWords, first]);
     setRemainingWords(rest);
   };
+
+  const handleClickPrint = () => console.log(previousWords.join(", "));
 
   return (
     <div className="flex gap-4 mb-1">
       <button
         className="border-2 border-blue-500 rounded-md px-2 py-1"
-        onClick={handleClick}
+        onClick={handleClickNext}
       >
         Next
+      </button>
+      <button
+        className="border-2 border-blue-500 rounded-md px-2 py-1"
+        onClick={handleClickPrint}
+      >
+        Print
       </button>
       <div className="text-center text-2xl font-bold">{word}</div>
     </div>
